fa90467be858a9311aa9b4f152e3aa53effa1ecc
Maysn <mmazen1321@gmail.com>
2021-01-19 21:27:07 +0200

    forgot one of the triple equal a7a

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
           </div>
         </div>
         <div className={"grey"}>
-          { this.state.toDoList.length==0 && <div className='Tip'><span><FaEnvelope style={{fontSize:'50px', color:'#CACFD6'}} /></span></div>}
+          { this.state.toDoList.length===0 && <div className='Tip'><span><FaEnvelope style={{fontSize:'50px', color:'#CACFD6'}} /></span></div>}
           {this.state.toDoList.length > 0 && (
             <div className={"theList"}>
               <h2>Told you not to..Now get to work!</h2>
